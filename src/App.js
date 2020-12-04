2f5ec9dbd5b9e943b1f66251231b1ed30cd31e8b
Dan Lubbers <lubbers.dan@gmail.com>
2020-12-04 14:51:52 -0500

    APIs are in .env and data is pulling from Contentful with graphQL

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const query = `{
+  titleCollection {
+    items {
+      title
+      logo {
+        title
+        description
+        contentType
+        fileName
+        size
+        url
+        width
+        height
+      }
+    }
+  }
+}`;
 
 function App() {
+  const graphqlURL = `https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/`;
+  const ContentfulAPI = process.env.REACT_APP_CONTENTFUL_CONTENT_DELIVERY_API;
+
+  const [page, setPage] = useState(null);
+
+  useEffect(() => {
+    fetch(graphqlURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${ContentfulAPI}`,
+      },
+      body: JSON.stringify({ query }),
+    })
+      .then((res) => res.json())
+      .then(({ data, errors }) => {
+        console.log(data);
+        setPage(data.titleCollection.items[0]);
+        if (errors) console.error(errors);
+      });
+  }, []);
+
+  if (!page) return "Loading...";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={page.logo.url} className="App-logo" alt="logo" />
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {page.title}
         </a>
       </header>
     </div>
